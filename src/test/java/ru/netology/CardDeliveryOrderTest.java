697d6dd2e7cdb5060c53220c70b1d2f536ad8d14
asachiyigor <asachiyigor@gmail.com>
2022-05-14 18:40:52 +0300

    version 2

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -4,20 +4,33 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
+    private LocalDate today = LocalDate.now();
+    private int days = 7;
+    private LocalDate dDay = LocalDate.now()
+                                      .plusDays(days);
+    private int onlyDate = dDay.getDayOfMonth();
+    String onDaysDelivery = LocalDate.now()
+                                     .plusDays(days)
+                                     .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
-    public String generateDate(int days) {
-        return LocalDate.now()
-                        .plusDays(days)
-                        .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    public boolean isMonthNotOk() {
+
+        if (today.getMonth() != dDay.getMonth()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Test
@@ -30,33 +43,44 @@ public class CardDeliveryOrderTest {
         $(".checkbox__box").click();
         $(withText("Забронировать")).click();
         $(withText("Успешно!")).should(visible);
+
     }
 
     @Test
-    public void shouldSetCityBy2Char() {
+    public void shouldSetCityBy2Char() throws InterruptedException {
         Configuration.timeout = 15000;
         open("http://localhost:9999/");
         $("[placeholder='Город']").setValue("Са");
-        $(withText("Санкт-Петербург")).click();
+        $$(".menu-item__control").findBy(text("Санкт-Петербург"))
+                                 .click();
+        $(".icon_name_calendar").click();
+        if (isMonthNotOk()) {
+            $("[data-step='1']").click();
+        }
+        $$(".calendar__day").findBy(text(String.valueOf(onlyDate)))
+                            .click();
         $("[name='name']").setValue("Иван Петров-Иванов");
         $("[name='phone']").setValue("+79119999999");
         $(".checkbox__box").click();
         $(withText("Забронировать")).click();
         $(withText("Успешно!")).should(visible);
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно забронирована на " + onDaysDelivery), Duration.ofSeconds(15));
     }
 
     @Test
     public void shouldSet7DaysAfterCurrentDate() {
-        String setDate = generateDate(7);
         Configuration.timeout = 15000;
         open("http://localhost:9999/");
         $("[placeholder='Город']").setValue("Санкт-Петербург");
         $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[placeholder='Дата встречи']").setValue(setDate);
+        $("[placeholder='Дата встречи']").setValue(onDaysDelivery);
         $("[name='name']").setValue("Иван Петров-Иванов");
         $("[name='phone']").setValue("+79119999999");
         $(".checkbox__box").click();
         $(withText("Забронировать")).click();
         $(withText("Успешно!")).should(visible);
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно забронирована на " + onDaysDelivery), Duration.ofSeconds(15));
     }
 }
